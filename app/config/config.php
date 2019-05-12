fd921db458fe181cb0d63b7cb03aed9f7722fae8
enidovasku <enidovasku@gmail.com>
2019-05-12 13:01:02 +0200

    model creation and pdo wrapper to access db

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+//Db params
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','admin');
+define('DB_NAME','riodmvc');
+
+// App root
+define('APPROOT',dirname(dirname(__FILE__)));
+
+// url root 
+define('URLROOT','http://localhost:8001/phpframework');
+
+// site name 
+define('SITENAME','PHPframework');
+
